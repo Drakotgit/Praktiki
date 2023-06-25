2321e3d897a9079aeb7c5569c52cc473e40d8a64
Drakotgit <137714007+Drakotgit@users.noreply.github.com>
2023-06-25 20:46:00 +0300

    Update N2.jl

--- Praktika_2/N2.jl
+++ Praktika_2/N2.jl
@@ -3,5 +3,5 @@ include("N1.jl")
 one(::Type{Matrix{T}}) where T = [1 0; 0 1]
 
 function fib(n :: Int)
-    return (fastpow([0 1; 1 1],n)*[0 ; 1])[1,1]
+    return (fastpow([0 1; 1 1], n)*[0 ; 1])[1,1]
 end
