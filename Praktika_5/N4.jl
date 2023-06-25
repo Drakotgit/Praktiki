9dc330d8e57341c239c71e6cfb3a738d8ab37bac
Drakotgit <137714007+Drakotgit@users.noreply.github.com>
2023-06-25 21:41:12 +0300

    Create N4.jl

---
+++ Praktika_5/N4.jl
@@ -0,0 +1,38 @@
+include ("N3.jl")
+
+@inline function _merge!(arr, arr1, arr2)
+    i1 :: Int = 1
+    i2 :: Int = 1
+
+    while i1+i2-1<=length(arr)
+@inbounds   if i2>length(arr2) || i1<=length(arr1) && arr1[i1]<=arr2[i2]
+@inbounds       arr[i1+i2-1] = arr1[i1]
+                i1+=1
+                continue
+        end
+@inbounds        if i1>length(arr1) || i2<=length(arr2) && arr1[i1]>=arr2[i2]
+@inbounds            arr[i1+i2-1] = arr2[i2]
+                    i2+=1
+                    continue
+        end
+    end
+    return arr
+end
+
+function _MergeSort(arr :: AbstractArray{T}) where T
+    _arr = similar(arr)
+    step :: Int = 1
+    while step<=length(arr)
+        i :: Int = 1
+        while true
+            border = min(i+2*step-1,length(arr))
+            if i+step>border break end
+  @inbounds @views _merge!(_arr[i:border],arr[i:i+step-1], arr[i+step:border])
+  @inbounds arr[i:border] .= @view(_arr[i:border])
+            if border == length(arr) break end
+            i = i+2step
+        end
+        step*=2
+    end
+    return arr
+end
