b5274ccc6106a868690919ef5002fb796317bbcc
Drakotgit <137714007+Drakotgit@users.noreply.github.com>
2023-06-25 21:37:28 +0300

    Create N3.jl

---
+++ Praktika_5/N3.jl
@@ -0,0 +1,16 @@
+include("N2.jl")
+
+@inline function _ShellSort(arr :: AbstractArray{T}) where T
+    step = length(arr)
+    while step>=1
+        for i in 2:length(arr)
+            j :: Int = i
+            @inbounds while j-step>0 && arr[j-step]>arr[j]
+                @inbounds arr[j-step], arr[j] = arr[j], arr[j-step]
+                j-=step
+            end
+        end
+        step÷=2
+    end
+    return arr
+end
