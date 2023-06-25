70a277dff9d216b90428df95eea1e40e653d85e3
Drakotgit <137714007+Drakotgit@users.noreply.github.com>
2023-06-25 21:31:27 +0300

    Create N5.jl

---
+++ Praktika_4/N5.jl
@@ -0,0 +1,21 @@
+function to_step(_A :: AbstractMatrix{T}; eps = 1e-3) where T
+    function swap!(B :: AbstractArray{T}, C :: AbstractArray{T})
+        @inbounds for i in eachindex(B,C)
+            B[i], C[i] = C[i], B[i]
+        end
+    end
+    @inline function checkdif!(B :: AbstractVector{T}, C :: AbstractVector{T}, koeff :: T)
+        @inbounds for i in eachindex(B)
+            B[i] = (abs(B[i] - koeff*C[i]) > eps ? B[i] - koeff*C[i] : 0)
+        end
+    end
+    A :: AbstractMatrix{T} = copy(_A)
+    @inbounds for row in 1:size(A, 1)
+        maxval, rowindex = findmax(abs, @view(A[row:end,row]))
+        swap!(@view(A[row,:]), @view(A[row + rowindex - 1,:]))
+        for nextrow in row+1:size(A,1)
+            @views checkdif!(A[nextrow,row:end],A[row,row:end],A[nextrow,row]/A[row,row])
+        end
+    end
+    return A
+end
