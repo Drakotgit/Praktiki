1010de933037d54cb6f49084366635e52306962b
Drakotgit <137714007+Drakotgit@users.noreply.github.com>
2023-06-25 21:33:15 +0300

    Create N8.jl

---
+++ Praktika_4/N8.jl
@@ -0,0 +1,15 @@
+include("N5.jl")
+
+function rang(_M :: AbstractMatrix{T}) where T
+    M = to_step(_M);
+    i :: Int, j :: Int = 1, 1
+    rangM :: Int = 0
+    while i<=size(M,2) && j<=size(M,1)
+        if !iszero(M[j,i])
+            rangM+=1;
+            j+=1;
+        end
+        i+=1;
+    end
+    return rangM;
+end
