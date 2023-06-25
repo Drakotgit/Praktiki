f3b5879a8507e58c51cd67a66004af5343d0005b
Drakotgit <137714007+Drakotgit@users.noreply.github.com>
2023-06-25 21:33:46 +0300

    Create N9.jl

---
+++ Praktika_4/N9.jl
@@ -0,0 +1,11 @@
+include("N5.jl")
+
+function det(_M :: AbstractMatrix{T} ; eps = 1e-5) where T
+    @assert size(_M,1) == size(_M,2)
+    M = to_step(_M)
+    detM :: T = one(T)
+    for i in 1:size(M,1)
+        detM*=M[i,i]
+    end
+    return detM
+end
