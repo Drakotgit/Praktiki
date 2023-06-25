bb76fd3d04502015dd6b5869ce93640dca2fa04f
Drakotgit <137714007+Drakotgit@users.noreply.github.com>
2023-06-25 20:28:44 +0300

    N2.jl

---
+++ Praktika_1/N2.jl
@@ -0,0 +1,12 @@
+include("N1")
+
+function gcdx(a, b, c)
+    x1, x2, y1, y2 = c, 0, 0, c
+    while b>0
+        k=div(a, b)
+        a, b = b, a%b
+        x1, x2 = x2, x1-x2*k
+        y1, y2, = y2, y1-y2*k
+    end
+    return a, x1, y1
+end
